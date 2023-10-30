65d79e5146a908c72bfe5ad3047d1515ae6ca0fc
thanhtin4401 <thanhtin4401.ufm@gmail.com>
2023-10-30 22:52:21 +0700

    update start

--- components/headersection/HeaderSection.tsx
+++ components/headersection/HeaderSection.tsx
@@ -6,22 +6,6 @@ import "animate.css";
 import SkillParalax from "../skillparalax/SkillParalax";
 import Image from "next/image";
 import CardBlog from "../cardblog/CardBlog";
-const menuLinkVars = {
-  initial: {
-    y: "30vh",
-    transition: {
-      duration: 0.2,
-      ease: [0.12, 0, 0.39, 0],
-    },
-  },
-  open: {
-    y: 0,
-    transition: {
-      duration: 0.5,
-      ease: [0.22, 1, 0.36, 1],
-    },
-  },
-};
 
 const HeaderSection = () => {
   const ref = useRef(null);
@@ -106,16 +90,33 @@ const HeaderSection = () => {
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{
                   duration: 0.8,
-                  delay: 0.5,
+                  delay: 2.5,
                   ease: [0, 0.71, 0.2, 1.01],
                 }}
               >
-                <h1
-                  className={`${styles.text_name} text-[2rem] lg:text-[2.5rem] font-bold text_name`}
-                >
-                  THANH TIN{" "}
-                  <span className={`${styles.text_title} `}>DEVELOP</span>
-                </h1>
+                <div className="flex items-center space-x-4">
+                  {" "}
+                  <h1
+                    className={`${styles.text_name} text-[2rem] lg:text-[2.5rem] font-bold text_name`}
+                  >
+                    THANH TIN{" "}
+                    <span className={`${styles.text_title} `}> DEVELOP</span>
+                  </h1>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="yellow"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="yellow"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                    />
+                  </svg>
+                </div>
 
                 <p className="text-white  text-left  font-light">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -152,8 +153,36 @@ const HeaderSection = () => {
                   </div>
                 </div>
                 <div className="flex space-x-2">
-                  <CardBlog url="https://res.cloudinary.com/dvzingci9/video/upload/v1665504047/airBnB/video/X2Download.app-_4K_HDR_NON_NUOC_CAO_BANG_UNESCO_GLOBAL_GEOPARK_VIETNAM.-_1080p_ruoqf8.mp4" />
-                  <CardBlog url="https://res.cloudinary.com/dvzingci9/video/upload/v1665504047/airBnB/video/X2Download.app-_4K_HDR_NON_NUOC_CAO_BANG_UNESCO_GLOBAL_GEOPARK_VIETNAM.-_1080p_ruoqf8.mp4" />
+                  <motion.div
+                    className="animate__zoomInDown mb:w-full sm:w-full xl:w-2/4 
+                          flex
+                        
+                        "
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: 2.1,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <CardBlog url="https://res.cloudinary.com/dvzingci9/video/upload/v1665504047/airBnB/video/X2Download.app-_4K_HDR_NON_NUOC_CAO_BANG_UNESCO_GLOBAL_GEOPARK_VIETNAM.-_1080p_ruoqf8.mp4" />
+                  </motion.div>
+                  <motion.div
+                    className="animate__zoomInDown mb:w-full sm:w-full xl:w-2/4 
+                          flex
+                        
+                        "
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: 2.1,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                  >
+                    <CardBlog url="https://res.cloudinary.com/dvzingci9/video/upload/v1665504047/airBnB/video/X2Download.app-_4K_HDR_NON_NUOC_CAO_BANG_UNESCO_GLOBAL_GEOPARK_VIETNAM.-_1080p_ruoqf8.mp4" />
+                  </motion.div>
                 </div>
               </motion.div>
 
